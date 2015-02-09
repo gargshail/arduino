ff84df1bf29feedc63b9322d8825d0e0f5b26ff2
Shailesh <garg.shailesh@gmail.com>
2015-02-09 14:34:56 -0800

    added comments

--- attiny/capacitiveSwitch/touchSwitch.ino
+++ attiny/capacitiveSwitch/touchSwitch.ino
@@ -3,6 +3,8 @@
  
 //pin 1 is sensor pin
 // place  a high value resistor between pins 0 & 1
+// extend a wire from pin 1. That will act as touch switch
+// add a LED on pin 3 to ground. You will be adding a relay if you are controlling a large load.
 CapacitiveSensor   sensor = CapacitiveSensor(0,1); 
 
 unsigned long lastTapped; 
